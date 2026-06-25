283126770df4fe0fce86a4046beb2f9bc0fc0b24
codexmish <majharulislam.shanto.70@gmail.com>
2026-06-25 15:51:17 +0600

    prisma database connected

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,20 @@
 import app from "./app";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 // import { prisma } from "./lib/prisma";
 const port = config.port;
 async function main() {
   try {
-    // await prisma.$connect();
-    // console.log("connected database");
+    await prisma.$connect();
+    console.log("connected database");
 
     app.listen(port, () => {
       console.log(`server is running on port ${port}`);
     });
   } catch (error) {
     console.error("error starting the server:", error);
-    // await prisma.$disconnect();
-    // process.exit(1);
+    await prisma.$disconnect();
+    process.exit(1);
   }
 }
 
