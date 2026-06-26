d9f098c7af050457fd1908890eeb2dd5ca66407f
codexmish <majharulislam.shanto.70@gmail.com>
2026-06-26 12:50:17 +0600

    signup/register controller added

--- server/src/models/auth/auth.controller.ts
+++ server/src/models/auth/auth.controller.ts
@@ -1,9 +1,26 @@
 import { Request, Response } from "express";
 import { authServices } from "./auth.services";
+import sendRes from "../../helpers/SendResponse";
+import httpStatus from "http-status";
 
 // -----register controller
 const signup = async (req: Request, res: Response) => {
-  const user = await authServices.signup(req.body);
+  try {
+    const user = await authServices.signup(req.body);
+
+    sendRes(res, {
+      statusCode: httpStatus.CREATED,
+      success: true,
+      message: "user created successfully",
+      data: user,
+    });
+  } catch (error: any) {
+    sendRes(res, {
+      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      success: false,
+      message: error.message,
+    });
+  }
 };
 
 export const authController = {
